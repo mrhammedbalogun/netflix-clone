a4db4ace0ae012c7d57712065d9af8f39edf69ec
Balogun Hammed Olasunkanmi <mrhammedbalogun@gmail.com>
2021-07-07 23:26:15 +0100

    Initial commit

---
+++ netflix_clone/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+app_name = "accounts"
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
+
+]
